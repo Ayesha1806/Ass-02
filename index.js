b6646dd0aeb77a85c0df5a03bb9f528438931d14
Ayesha1806 <96306552+Ayesha1806@users.noreply.github.com>
2023-04-05 23:11:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function multiplyByTwo(num) {
+    return num * 2;
+  }
+  function divideByTwo(num) {
+    return num / 2;
+  }
+  function findprime(num) {
+    for(let i = 2; i <= num; i++) {
+      let isPrime = true;
+      
+      for(let j = 2; j <= Math.sqrt(i); j++) {
+        if(i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      
+      if(isPrime) {
+        console.log(i);
+      }
+    }
+  }
+  
+  findprime(97);
+      
